9f9c959ea838479e9061bae73b78771842c91b83
SAIRAM299 <psairam299@gmail.com>
2024-04-25 04:18:53 -0700

    FINAL

--- Client/src/Component/protectorRouter.js
+++ Client/src/Component/protectorRouter.js
@@ -89,7 +89,7 @@ const unshow=()=>{
                             <li className='navitem'><a href='' className=' navlink'>About</a></li>
                             <li className='navitem'><a href='' className=' navlink'>Contact</a></li>
 
-                            <li className='navitem'><a href='' className=' navlink'><button className='btns' >User:
+                            <li className='navitem'><a href='' className=' navlink'><button className='btns' onClick={log} >User:
                                 {user.name}
                                 
                                  </button></a></li>
